bcdf8fa3f9c9cbf33558b948536cc5a113db574d
manuelfjr <ferreira.jr.ufpb@gmail.com>
2023-07-03 16:59:10 -0300

    update: new results

--- pipeline/run.py
+++ pipeline/run.py
@@ -46,7 +46,7 @@ if parameters["include_random_model"]:
 else:
     number_random_models = 1
 path_result = Path(config.dir_result)
-step_pause = 7
+step_pause = 25
 
 for k_random in tqdm(range(step_pause, number_random_models)):
     #     if k_random > 0:

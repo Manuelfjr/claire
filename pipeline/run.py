215a9a56cfb5c2ef205e5cb8cc413e5db226b526
Manuel <ferreira.jr.ufpb@gmail.com>
2023-07-04 18:47:14 -0300

    update: add more results 17>

--- pipeline/run.py
+++ pipeline/run.py
@@ -47,7 +47,7 @@ else:
     number_random_models = 1
 path_result = Path(config.dir_result)
 
-step_pause = 0
+step_pause = 16
 del config.params["optics"]
 for k_random in tqdm(range(step_pause, number_random_models)):
 #     if k_random > 0:

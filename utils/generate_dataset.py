2ccaeeb9e2aad1ce892fdf707227311b6188279a
manuelfjr <ferreira.jr.ufpb@gmail.com>
2023-07-06 13:58:52 -0300

    (rpa-13:58:50) update: more results

--- utils/generate_dataset.py
+++ utils/generate_dataset.py
@@ -1,4 +1,6 @@
 # basics
+import argparse
+import os
 import sys
 from pathlib import Path
 
@@ -7,20 +9,15 @@ sys.path.append(str(PROJECT_DIR))
 
 import numpy as np
 import pandas as pd
+
+# utils
+from reader import read_file_yaml
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
 
-# utils
-import argparse
-import os
-
 # basics
 
-file_path_parameters = (
-    PROJECT_DIR 
-    / "conf" 
-    / "parameters"
-) 
+file_path_parameters = PROJECT_DIR / "conf" / "parameters.yml"
 np.random.seed(0)
 
 # args
@@ -41,104 +38,74 @@ parser.add_argument(
     default=[file_path_parameters],
     help=f"file path to parameters configuration. (default: {file_path_parameters})",
 )
+parser.add_argument(
+    "random_state",
+    metavar="RANDOM_STATE",
+    type=int,
+    nargs="?",
+    default=170,
+    help=f"set random_state for data generation (default: 170)",
+)
 args = parser.parse_args()
+
+# ============
+# read parameters
+parameters = read_file_yaml(file_path_parameters)
+
+_params_dataset = {
+    i_name: {
+        "params": i_content,
+    }
+    for i_name, i_content in parameters["generation_params"].items()
+}
+# ============
 
 # ============
 # Generate datasets. We choose the size big enough to see the scalability
 # of the algorithms, but not too big to avoid too long running times
 # ============
-noisy_circles = datasets.make_circles(n_samples=args.integers, factor=0.5, noise=0.05)
-noisy_moons = datasets.make_moons(n_samples=args.integers, noise=0.05)
-blobs = datasets.make_blobs(n_samples=args.integers, random_state=8)
-no_structure = np.random.rand(1000, 2), None
+content = {}
+noisy_circles = datasets.make_circles(n_samples=args.integers, **_params_dataset["noisy_circles"]["params"])
+noisy_moons = datasets.make_moons(n_samples=args.integers, **_params_dataset["noisy_moons"]["params"])
+blobs = datasets.make_blobs(n_samples=args.integers, **_params_dataset["blobs"]["params"])
+no_structure = np.random.rand(*_params_dataset["no_structure"]["params"]), None
 
 # Anisotropicly distributed data
-random_state = 170
-X, y, *others = datasets.make_blobs(n_samples=args.integers, random_state=random_state)
-transformation = [[0.6, -0.6], [-0.4, 0.8]]
-X_aniso = np.dot(X, transformation)
+X, y, *others = datasets.make_blobs(n_samples=args.integers, random_state=args.random_state)
+rotation = _params_dataset["aniso"]["params"]["rotation"]
+X_aniso = np.dot(X, rotation)
 aniso = (X_aniso, y)
 
 # blobs with varied variances
-varied = datasets.make_blobs(n_samples=args.integers, cluster_std=[1.0, 2.5, 0.5], random_state=random_state)
+varied = datasets.make_blobs(
+    n_samples=args.integers, random_state=args.random_state, **_params_dataset["varied"]["params"]
+)
 
-plot_num = 1
-
-default_base = {
-    "quantile": 0.3,
-    "eps": 0.3,
-    "damping": 0.9,
-    "preference": -200,
-    "n_neighbors": 3,
-    "n_clusters": 3,
-    "min_samples": 7,
-    "xi": 0.05,
-    "min_cluster_size": 0.1,
+# organize content
+content = {
+    "aniso": aniso,
+    "noisy_circles": noisy_circles,
+    "noisy_moons": noisy_moons,
+    "blobs": blobs,
+    "varied": varied,
+    "no_structure": no_structure,
 }
 
-datasets = [
-    (
-        noisy_circles,
-        "noisy_circles",
-        {
-            "damping": 0.77,
-            "preference": -240,
-            "quantile": 0.2,
-            "n_clusters": 2,
-            "min_samples": 7,
-            "xi": 0.08,
-        },
-    ),
-    (
-        noisy_moons,
-        "noisy_moons",
-        {
-            "damping": 0.75,
-            "preference": -220,
-            "n_clusters": 2,
-            "min_samples": 7,
-            "xi": 0.1,
-        },
-    ),
-    (
-        varied,
-        "varied",
-        {
-            "eps": 0.18,
-            "n_neighbors": 2,
-            "min_samples": 7,
-            "xi": 0.01,
-            "min_cluster_size": 0.2,
-        },
-    ),
-    (
-        aniso,
-        "aniso",
-        {
-            "eps": 0.15,
-            "n_neighbors": 2,
-            "min_samples": 7,
-            "xi": 0.1,
-            "min_cluster_size": 0.2,
-        },
-    ),
-    (blobs, "blobs", {"min_samples": 7, "xi": 0.1, "min_cluster_size": 0.2}),
-    (no_structure, "no_structure", {}),
-]
-
+_datasets = {
+    i_name: {
+        "content": content[i_name],
+    }
+    for i_name, i_content in parameters["generation_params"].items()
+}
 
 dataset_std = []
-for i_dataset, (dataset, name, algo_params) in enumerate(datasets):
-    URL = (
-        PROJECT_DIR
-        / "data" 
-        / name
-        )
-    X, y = dataset
+data = {}
+for i_name, args in _datasets.items():
+    URL = PROJECT_DIR / "data" / i_name
+    X, y = args["content"]
     X = StandardScaler().fit_transform(X)
     if not os.path.exists(URL):
         os.makedirs(URL)
-    data = pd.DataFrame(X)
-    data["labels"] = y
-    #data.to_csv(os.path.join(URL, name + ".csv"), index=False)
-print(data)
+    data[i_name] = pd.DataFrame(X)
+    data[i_name]["labels"] = y
+    data[i_name].to_csv(URL / Path(i_name + ".csv"), index=False)

3a8816914ca0ca73edb7bfeabc9e17b51a607078
Manuel <ferreira.jr.ufpb@gmail.com>
2023-07-20 21:34:31 -0300

    (rpa-21:34:23) update: more results

--- utils/generate_dataset.py
+++ utils/generate_dataset.py
@@ -56,6 +56,7 @@ args = parser.parse_args()
 # ============
 # read parameters
 parameters = read_file_yaml(file_path_parameters)
+ext_type = params["outputs"]["extension_type"]
 
 _params_dataset = {
     i_name: {
@@ -159,7 +160,7 @@ for i_name, args in _datasets.items():
     (X, y), (X_pca, y_pca) = args["content"]
     if (X_pca is None) and (y_pca is None):
         data[i_name] = transform_and_input_target(URL, i_name, X, y, data)
-        data[i_name].to_csv(URL / Path(i_name + ".csv"), index=False)
+        data[i_name].to_csv(URL / Path(i_name + ext_type), index=False)
     else:
         data[i_name] = transform_and_input_target(URL, i_name, X_pca, y_pca, data)
-        data[i_name].to_csv(URL / Path(i_name + "_pca.csv"), index=False)
+        data[i_name].to_csv(URL / Path(i_name + "_pca" + ext_type), index=False)

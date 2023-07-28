07db45b84b462bce3866159d8e2e0cf7cbb4bd0b
manuel <ferreira.jr.ufpb@gmail.com>
2023-07-28 16:11:16 -0300

    (rpa-16:11:16) update: more results

--- pipeline/run.py
+++ pipeline/run.py
@@ -1,6 +1,8 @@
+import concurrent.futures
 import os
 import sys
 import warnings
+from functools import partial
 from pathlib import Path
 
 import numpy as np
@@ -35,8 +37,99 @@ ext_best_img = parameters["outputs"]["extension_best_img"]
 path_result = PROJECT_DIR / config.dir_result
 if not os.path.exists(path_result):
     os.makedirs(path_result)
-##### read       ##################################
+#### read        ##################################
 data_all = {i: pd.read_csv(path_data[idx] / Path(i + ext_type)) for idx, i in enumerate(config.file_names)}
+
+
+#### methods     ##################################
+def process_file(k_random, path_result_k_random, _X, _Y, config, data_random, parameters, i):
+    models_params = config.params | {"optics": [config._optics_params[i]]}
+
+    claire = CLAIRE(
+        models_name=np.unique(config.models_name_dataset[i]),
+        models=config.models,
+        params=models_params,
+        _X=_X,
+        _Y=_Y,
+        metrics=config.metrics,
+        dir_result=path_result_k_random,
+        path_root=PROJECT_DIR,
+    )
+
+    if len(np.unique(_Y[i])) == 1:
+        n_clusters = np.random.randint(1, 10)
+    else:
+        n_clusters = len(np.unique(_Y[i]))
+
+    which_k_dataset = "dataset: [ {} ]".format(i)
+    print(title_part_n1 + which_k_dataset + title_part_n3)
+
+    #  processing
+    combination_models = claire.transform()
+    claire.fit_combination_models(combination_models, _X[i])
+    data_results = claire.generate_results(combination_models)
+
+    # add random columns
+    data_results = pd.concat([data_results, data_random[i].iloc[:, : (k_random + 1)]], axis=1)
+    pij = claire.generate_pij_matrix(data_results, k_random + 1, n_clusters)
+
+    # set beta4 params
+    beta_params = parameters["beta_params"] | {"pij": pij, "n_respondents": pij.shape[1], "n_items": pij.shape[0]}
+
+    # fit
+    beta4_model = claire.fit_beta4(**beta_params)
+
+    # metrics
+    data_metrics = claire.calculate_metrics(data_results, beta4_model, claire._X[i], claire._Y[i])
+
+    # contents
+    dir_contents = [
+        (
+            "metrics",
+            (
+                "metrics" + ext_type,
+                data_metrics.sort_values("abilities", ascending=False),
+            ),
+            (None),
+        ),
+        (
+            "pij",
+            ("pij_true" + ext_type, pij),
+            ("pij_pred" + ext_type, pd.DataFrame(claire.b4.pij, columns=pij.columns)),
+        ),
+        (
+            "params",
+            (
+                "abilities" + ext_type,
+                pd.DataFrame(claire.b4.abilities, index=pij.columns, columns=["abilities"]),
+            ),
+            (
+                "diff_disc" + ext_type,
+                pd.DataFrame(
+                    {
+                        "difficulty": claire.b4.difficulties,
+                        "discrimination": claire.b4.discriminations,
+                    }
+                ),
+            ),
+        ),
+        ("labels", ("labels" + ext_type, data_results), (None)),
+    ]
+    #  save
+    claire.save_results(i, dir_contents)
+
+
+def process_k_random(k_random, init_generate, stop_generate, path_result, _X, _Y, config, data_random, parameters):
+    which_k_random = "n_random_model: [ {} ]".format(k_random + 1)
+    print(title_part_n1 + which_k_random + title_part_n3)
+    if number_random_models != 1:
+        path_result_k_random = path_result / Path(f"random_n{k_random+1}")
+        if not os.path.exists(path_result_k_random):
+            os.makedirs(path_result_k_random)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=len(config.file_names)) as executor:
+        process_func = partial(process_file, k_random, path_result_k_random, _X, _Y, config, data_random, parameters)
+        executor.map(process_func, config.file_names)
+
 
 #### running     ##################################
 _X, _Y = {}, {}
@@ -76,91 +169,4 @@ for i in config.file_names:
 
 del config.params["optics"]
 for k_random in tqdm(range(init_generate, stop_generate)):
-    #     if k_random > 0:
-    #         break
-    which_k_random = "n_random_model: [ {} ]".format(k_random + 1)
-    print(title_part_n1 + which_k_random + title_part_n3)
-    if number_random_models != 1:
-        path_result_k_partition = path_result / Path(f"random_n{k_random+1}")
-        if not os.path.exists(path_result_k_partition):
-            os.makedirs(path_result_k_partition)
-
-    for i in tqdm(config.file_names):
-        models_params = config.params | {"optics": [config._optics_params[i]]}
-
-        claire = CLAIRE(
-            models_name=np.unique(config.models_name_dataset[i]),
-            models=config.models,
-            params=models_params,
-            _X=_X,
-            _Y=_Y,
-            metrics=config.metrics,
-            dir_result=path_result_k_partition,
-            path_root=PROJECT_DIR,
-        )
-
-        if len(np.unique(_Y[i])) == 1:
-            n_clusters = np.random.randint(1, 10)
-        else:
-            n_clusters = len(np.unique(_Y[i]))
-
-        which_k_dataset = "dataset: [ {} ]".format(i)
-
-        print(title_part_n1 + which_k_dataset + title_part_n3)
-
-        #  processing
-        combination_models = claire.transform()
-        claire.fit_combination_models(combination_models, _X[i])
-
-        data_results = claire.generate_results(combination_models)
-
-        # add random columns
-        data_results = pd.concat([data_results, data_random[i].iloc[:, : (k_random + 1)]], axis=1)
-
-        pij = claire.generate_pij_matrix(data_results, k_random + 1, n_clusters)
-
-        # set beta4 params
-        beta_params = parameters["beta_params"] | {"pij": pij, "n_respondents": pij.shape[1], "n_items": pij.shape[0]}
-
-        # fit
-        beta4_model = claire.fit_beta4(**beta_params)
-
-        # metrics
-        data_metrics = claire.calculate_metrics(data_results, beta4_model, claire._X[i], claire._Y[i])
-
-        # contents
-        dir_contents = [
-            (
-                "metrics",
-                (
-                    "metrics" + ext_type,
-                    data_metrics.sort_values("abilities", ascending=False),
-                ),
-                (None),
-            ),
-            (
-                "pij",
-                ("pij_true" + ext_type, pij),
-                ("pij_pred" + ext_type, pd.DataFrame(claire.b4.pij, columns=pij.columns)),
-            ),
-            (
-                "params",
-                (
-                    "abilities" + ext_type,
-                    pd.DataFrame(claire.b4.abilities, index=pij.columns, columns=["abilities"]),
-                ),
-                (
-                    "diff_disc" + ext_type,
-                    pd.DataFrame(
-                        {
-                            "difficulty": claire.b4.difficulties,
-                            "discrimination": claire.b4.discriminations,
-                        }
-                    ),
-                ),
-            ),
-            ("labels", ("labels" + ext_type, data_results), (None)),
-        ]
-
-        # save
-        claire.save_results(i, dir_contents)
+    process_k_random(k_random, init_generate, stop_generate, path_result, _X, _Y, config, data_random, parameters)
